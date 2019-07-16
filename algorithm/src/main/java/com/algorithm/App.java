b6e2a6d97d5178fa79a6f4dbae6a05d1a4f7794b
mzxl1987 <448632824@qq.com>
2019-07-16 18:55:02 +0800

    1.算法

---
+++ algorithm/src/main/java/com/algorithm/App.java
@@ -0,0 +1,16 @@
+package com.algorithm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Math.abs(-2147483648);
+        System.out.println( "Hello World!" + Math.abs(-2147483648L));
+        System.out.println(0xFFFFFFFF < 0);
+        System.out.println(-(-2147483648));
+    }
+}
